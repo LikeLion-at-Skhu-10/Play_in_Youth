b3ccf0b31f5fece2f01514edbf2a775bd42b5005
profitah <qwer120542@gmail.com>
2022-11-22 18:02:03 +0900

    Yoonah final commit

--- static/posts/js/loading.js
+++ static/posts/js/loading.js
@@ -1,23 +1,11 @@
-var slideWrapper = document.querySelector('.container');
-var slides = document.querySelectorAll('.item');
-var totalSlides = slides.length; 
+function setThumbnail(event) {
+    var reader = new FileReader();
 
+    reader.onload = function(event) {
+      var img = document.createElement("img");
+      img.setAttribute("src", event.target.result);
+      document.querySelector("div#PhotoPlusBox").appendChild(img);
+    };
 
-var sliderWidth = slideWrapper.clientWidth; 
-var slideIndex = 0;
-var slider = document.querySelector('.slider');
-
-slider.style.width = sliderWidth * totalSlides + 'px';
-
-showSlides()
-
-function showSlides() {
-    for(var i=0;i<slides.length;i++){
-        slider.style.left = -(sliderWidth * slideIndex) + 'px';    
-    }
-    slideIndex++;
-    if (slideIndex === totalSlides) {
-        slideIndex = 0;
-    }
-    setTimeout(showSlides, 1000); 
-}
+    reader.readAsDataURL(event.target.files[0]);
+  }
